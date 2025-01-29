4a5e53568ce950624de103a613d5db28dc4da6bf
4aDomka <128625282+4aDomka@users.noreply.github.com>
2025-01-29 15:47:14 +0500

    Delete core directory

--- core/view.php
+++
@@ -1,8 +0,0 @@
-<?php
-class View
-{
-	function generate($content_view, $template_view, $authorised = null, $data = null, $image = null, $token = null, $roles = null, $VKparams = null)
-	{
-		include 'views/' . $template_view;
-	}
-}
