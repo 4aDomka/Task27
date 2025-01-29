8789c998985077a491897b62af50d8c2ed4f1981
4aDomka <128625282+4aDomka@users.noreply.github.com>
2025-01-29 15:48:44 +0500

    Delete views directory

--- views/register.php
+++
@@ -1,5 +0,0 @@
-<form method="POST">
-	Логин <input name="login" type="text" required><br>
-	Пароль <input name="password" type="password" required><br>
-	<input name="submit" type="submit" value="Зарегистрироваться">
-</form>
