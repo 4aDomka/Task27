fe61cff33c511ca0d8cb2d47dfa200b2bd0a266c
4aDomka <128625282+4aDomka@users.noreply.github.com>
2025-01-29 15:46:46 +0500

    Delete config directory

--- config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'core/model.php';
-require_once 'core/view.php';
-require_once 'core/controller.php';
-require_once 'core/main.php';
-require_once 'config/configuration.php';
-require_once 'vendor/autoload.php';
-Main::showPage();
--- config/pages.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-'3' => 'Home',
-'2' => 'Register',
-'1' => 'Login',
-'4' => 'Unlogin',
-);
